4457480cba9a4cf6342fce5134ca6dd1d635c8f5
Dmitry-russ <dapetuhov@yandex.ru>
2023-01-13 21:51:23 +0500

    прописал основные моменты, надо сделать миграции

--- Plan/train/urls.py
+++ Plan/train/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path 
+from .views import train_list, cases_list, case_detail
 
-app_name = 'Plan'
+app_name = 'train'
 urlpatterns = [
-    path('', index, name='index'),
+    path('', train_list, name='train_list'),
+    path('train/<int:train_id>/', cases_list, name='cases_list'),
+    path('case/<int:case_id>/', case_detail, name='cases_detail'),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
-    path('group/<slug:slug>/', group_posts, name='group_list'),
+    # path('group/<slug:slug>/', group_posts, name='group_list'),
     # path('profile/<str:username>/', views.profile, name='profile'),
     # path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
     # path('create/', views.post_create, name='post_create'),
--- Plan/train/views.py
+++ Plan/train/views.py
@@ -2,16 +2,33 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.cache import cache_page
 
-from ..Plan.models import Image, Cases, Train, User
+from .models import Image, Cases, Train, User
 from .utils import page_control
 
 
-PAGE_LIST = 10
+PAGE_LIST = 30
 
-def index(request):
-    cases = Cases.objects.select_related('train')
+def train_list(request):
+    cases = Train.objects.select_related('cases')
     page_obj = page_control(request, cases, PAGE_LIST)
     context = {
         'page_obj': page_obj,
     }
-    return render(request, 'posts/index.html', context)
+    return render(request, 'train/train_list.html', context)
+
+def cases_list(request, id):
+    train = get_object_or_404(Train, id=id)
+    cases = train.cases
+    page_obj = page_control(request, cases, PAGE_LIST)
+    context = {
+        'page_obj': page_obj,
+        'train': train,
+    }
+    return render(request, 'train/cases_list.html', context)
+
+def case_detail(request, id):
+    case = get_object_or_404(Cases, id=id)
+    context = {
+        'case': case,
+    }
+    return render(request, 'train/case_detail.html', context)
