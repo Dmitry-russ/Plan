d130223af3540998cf6b29232832561f3374a24f
Dmitry-russ <dapetuhov@yandex.ru>
2023-01-15 21:56:35 +0500

    хосты

--- Plan/Plan/settings.py
+++ Plan/Plan/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =  os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['194.187.122.3', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*',]
 
 
 INSTALLED_APPS = [
