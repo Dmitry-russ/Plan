9c923db082bf817f4babc353fdd4b9df6bd32a6e
Dmitry-russ <dapetuhov@yandex.ru>
2023-02-19 00:26:04 +0500

    запускаю бота

--- bot/consts.py
+++ bot/consts.py
@@ -1,5 +1,5 @@
-TRAIN_ENDPOINT: str = 'http://127.0.0.1:8000/api/v1/train/list/'
-MAI_ENDPOINT: str = 'http://127.0.0.1:8000/api/v1/train/mai/'
-USER_ENDPOINT: str = 'http://127.0.0.1:8000/auth/jwt/create/'
+TRAIN_ENDPOINT: str = 'http://194.187.122.3/api/v1/train/list/'
+MAI_ENDPOINT: str = 'http://194.187.122.3/api/v1/train/mai/'
+USER_ENDPOINT: str = 'http://194.187.122.3/auth/jwt/create/'
 
 USERS: list = ['dmitrypetukhovekb', ]
