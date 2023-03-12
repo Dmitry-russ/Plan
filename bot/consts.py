d2fc0eb9bb8a3cfba7387f5a80668b677e3f99df
Dmitry-russ <dapetuhov@yandex.ru>
2023-03-12 17:35:24 +0500

    поправил бота и добавил возможность приложить файл

--- bot/consts.py
+++ bot/consts.py
@@ -1,14 +1,14 @@
-# TRAIN_ENDPOINT: str = 'http://194.187.122.3/api/v1/train/list/'
-# MAI_ENDPOINT: str = 'http://194.187.122.3/api/v1/train/mai/'
-# CASE_ENDPOINT: str = 'http://194.187.122.3/api/v1/train/case/'
-# USER_ENDPOINT: str = 'http://194.187.122.3/auth/jwt/create/'
-# MAI_NEXT_ENDPOINT: str = 'http://194.187.122.3/api/v1/train/maintenance/'
+TRAIN_ENDPOINT: str = 'http://194.187.122.3/api/v1/train/list/'
+MAI_ENDPOINT: str = 'http://194.187.122.3/api/v1/train/mai/'
+CASE_ENDPOINT: str = 'http://194.187.122.3/api/v1/train/case/'
+USER_ENDPOINT: str = 'http://194.187.122.3/auth/jwt/create/'
+MAI_NEXT_ENDPOINT: str = 'http://194.187.122.3/api/v1/train/maintenance/'
 
-TRAIN_ENDPOINT: str = 'http://127.0.0.1:8000/api/v1/train/list/'
-MAI_ENDPOINT: str = 'http://127.0.0.1:8000/api/v1/train/mai/'
-CASE_ENDPOINT: str = 'http://127.0.0.1:8000/api/v1/train/case/'
-USER_ENDPOINT: str = 'http://127.0.0.1:8000/auth/jwt/create/'
-MAI_NEXT_ENDPOINT: str = 'http://127.0.0.1:8000/api/v1/train/maintenance/'
+# TRAIN_ENDPOINT: str = 'http://127.0.0.1:8000/api/v1/train/list/'
+# MAI_ENDPOINT: str = 'http://127.0.0.1:8000/api/v1/train/mai/'
+# CASE_ENDPOINT: str = 'http://127.0.0.1:8000/api/v1/train/case/'
+# USER_ENDPOINT: str = 'http://127.0.0.1:8000/auth/jwt/create/'
+# MAI_NEXT_ENDPOINT: str = 'http://127.0.0.1:8000/api/v1/train/maintenance/'
 
 USERS: list = ['dmitrypetukhovekb',
                'smnvkrll',
